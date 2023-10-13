aed8d2462c2dd526a11d0dd5c97083ca1b389413
Nizzo07 <seu@email.com>
2023-10-13 17:18:57 +0100

    T

---
+++ RTestes/teste1/ex1.hs
@@ -0,0 +1,4 @@
+
+--Apresente uma defini¸c˜ao recursiva da fun¸c˜ao unlines :: [String] -> String que junta todas as strings da lista numa s´o, separando-as pelo caracter ’\n’.
+
+--Por exemplo, unlines ["Prog", "Func"] == "Prog\nFunc".
