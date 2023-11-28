c064a8b7de602d6a02a7ebfdeafe62a91ad6f989
Nizzo07 <afonso.martins8282@gmail.com>
2023-11-28 21:25:50 +0000

    Ficha5

--- Ficha5/Exes.hs
+++ Ficha5/Exes.hs
@@ -34,4 +34,84 @@
 
 --f) 
     deleteBy' :: (a -> a -> Bool) -> a -> [a] -> [a]
-    deleteBy' 
+    deleteBy' f _ [] = []
+    deleteBy' f n (h:t) = if f n h then t else h : deleteBy' f n t
+
+--g) 
+    sortOn' :: Ord b => (a -> b) -> [a] -> [a]
+    sortOn' f [] = [] 
+    sortOn' f (h:t) = aux f h (sortOn' f t)
+                    where aux :: Ord b => (a -> b) -> a -> [a] -> [a]
+                          aux f x [] = [x]
+                          aux f x (a:b) = if f x > f a then a : aux f x b else x : a : b
+
+--2) 
+
+    type Polinomio = [Monomio]
+    type Monomio = (Float, Int)
+
+    --Por exemplo: [(2,3), (3,4), (5,3), (4,5)] = 2x^3 + 3x^4 + 5x^3 + 4x^5
+
+--a) Seleciona os graus de um polinómio
+
+    selgrau :: Int -> Polinomio -> Polinomio 
+    selgrau n [] = []
+    selgrau n pol = filter(\x -> n == snd (x)) pol
+-- a função filter é uma função de ordem superior que recebe uma função de predicado e uma lista. Ela retorna uma lista. Ela retorna uma nova lista contendo 
+-- apenas os elementos da lista de entrada para os quais o predicado é verdadeiro
+
+--b) Conta quantos monómios existem em um poilinómio especifico
+
+    conta :: Int -> Polinomio -> Int 
+    conta n pol = foldr(\x conta -> if n == snd(x) then conta+1 else conta) 0 pol 
+    
+    conta' :: Int -> Polinomio -> Int 
+    conta' n p = length(filter (\x -> n == snd x) p)
+-- a função foldr é uma função de ordem superior que é usada para reduzir uma lista para um único valor. Ela toma uma função binária e um valor inicial
+-- como argumentos, junto com a lista que se deseja dobrar.
+
+--c) Diz o maior grau existente em um polinómio 
+
+    grau :: Polinomio -> Int 
+    grau pol = foldr(\x g -> if snd x > g then  snd(x) else g) 0 pol 
+
+--d) Calcula a derivada de um polinómio 
+
+    deriv :: Polinomio -> Polinomio 
+    deriv pol = let l = map(\(c,g) -> if g > 0 then (c*fromIntegral(g),g-1) else (0,0)) pol             
+                in filter(/=(0,0)) l 
+
+--e) Calcula o valor de um polinómio para um valor especifico de x 
+
+    calcula :: Float -> Polinomio -> Float
+    calcula x pol = foldr(\(c,g) soma -> c*(x^g) + soma) 0 pol 
+
+--f) Polinómio que retira de um polinómio os monómios de coeficiente zero
+
+    simp :: Polinomio -> Polinomio
+    simp pol = filter(\x -> fst(x) /= 0) pol
+
+--g) Calcula o resultado da multiplicação de um monómio por um polinómio 
+
+    mult :: Monomio -> Polinomio -> Polinomio
+    mult (cm,gm) pol = map(\x -> (fst(x)*cm, snd(x)*gm)) pol 
+
+--h) Ordena o polináomio por ordem crescente dos graus dos seus monomios
+
+    ordena :: Polinomio -> Polinomio
+    ordena pol = sortOn' snd pol 
+    
+
+    ordena' :: Polinomio -> Polinomio 
+    ordena' pol = foldr zux [] pol 
+            where aux :: Monomio -> Polinomio -> Polinomio
+                  aux (cm,gm) [] = [(cm,gm)]
+                  aux (cm,gm) ((cm2,gm2):t) = if gm < gm2 then (cm,gm) : (cm2,gm2) : t else (cm2,gm2) : aux (cm,gm) t
+                  
+--i) Normaliza um polinómio
+ 
+    normaliza :: Polinomio -> Polinomio 
+    normaliza l = let x = fromIntegral $ grau l in [((a/x),b) | (a,b) <- l]
+
+
+
